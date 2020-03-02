fbd5c990bc13e1ccb2c752940592eea43e2e830b
countyflipper <stefan.template1@gmail.com>
2020-03-01 20:02:35 -0500

    Feature: Added and test subtraction method and CSV

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,3 +1,21 @@
+from CsvReader import CsvReader
+
+
+def addition(a, b):
+    c = a + b
+    return c
+
+
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
+def mean(data):
+    mean = data
+    return mean
 
 
 class Calculator:
@@ -5,3 +23,22 @@ class Calculator:
 
     def __init__(self):
         pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
+        pass
+
+    def mean(self):
+        mean(self.data)
