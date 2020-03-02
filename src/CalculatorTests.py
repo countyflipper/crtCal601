44920c6ba51e5574ce2cb03120c168e73e5f4385
countyflipper <stefan.template1@gmail.com>
2020-03-02 11:31:03 -0500

    Task: Added all CSV files

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,7 +11,13 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-        print("Hello World")
+    def test_square(self):
+        test_data = CsvReader('/src/Square.csv').data
+        for row in test_data:
+            self.assertAlmostEqual(self.calculator.square(int(row['Value 1']),int(row['Result'])))
+            self.assertAlmostEqual(self.calculator.result, int(row['Result']))
+            print(self.calculator.result)
+        print("------------------Square-------------------")
 
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
